006275f444bc2efae65df9adb61a9eb22d24fef3
Tay <taylor.mleigh@gmail.com>
2020-02-02 12:14:24 -0600

    popup gallery styled

--- client/src/components/PopUpGallery.jsx
+++ client/src/components/PopUpGallery.jsx
@@ -8,26 +8,27 @@ const PopUpGallery = ({images, id, currentPhoto, clickIt}) => {
     <>
       <div className="popup">
         <div className="popupgallery">
-            <button onClick={clickIt}>X</button>
-          <h2>{images.productName}</h2>
+          <button onClick={clickIt}>X</button>
+          <h4>{images.productName}</h4>
+          <br></br>
+          <div className="popupMain">
+            <FullImage 
+              className="popupprodImage" 
+              src={currentPhoto ? currentPhoto : images.imgUrls[0]}
+            />
+          </div>
           <div className="popupthumbnailsDiv">
             {
               images.imgUrls.map( (url, index) => {
                 return (
                   <Thumbnail 
                     className="popupthumbnail" 
-                    key={index, id} 
+                    key={index+id} 
                     image={url} 
                     width="100px"/>
                 )
               })
             }
-
-            <FullImage 
-              className="popupprodImage" 
-              src={currentPhoto ? currentPhoto : images.imgUrls[0]}
-            />
-
           </div>
         </div>
       </div>
