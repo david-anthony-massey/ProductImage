8b0b22e009583dc6316859f91e9c99d24d3bde32
David M <d.anthony.massey@gmail.com>
2020-04-01 06:08:51 -0500

    reconfigure for Heroku

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../database');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("../database");
+const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/dist"));
 
 //response to componentDidMount pulls image info from db based on product id
-app.get('/getImages', (req, res) => {
+app.get("/getImages", (req, res) => {
   db.getImage(req.query.productId, (err, data) => {
     if (err) {
-      console.error('server get error');
+      console.error("server get error");
     } else {
       res.send(data);
     }
-  })
+  });
 });
 
 app.listen(PORT, () => {
